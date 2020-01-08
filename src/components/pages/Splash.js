420f282d064eb494376e5c6a55ac97d8035b6dc6
UncleJerry <jared.myhrberg@gmail.com>
2020-01-08 10:40:33 -0800

    basically working everything

--- src/components/pages/Splash.js
+++ src/components/pages/Splash.js
@@ -3,9 +3,13 @@ import styles from './Splash.css';
 import usePlaylistEmitters from '../hooks/playlistState';
 import { usePlayListState } from '../../socket';
 import YouTube from 'react-youtube';
+import YTSearch from 'youtube-api-search';
+
+const superSecretCode = 'AIzaSyBwTEj5b5Vol3PHBtF2ZzChHSzbSNpk24c';
 
 const Splash = () => {
   const [link, setLink] = useState(null);
+  const [searchList, setSearchList] = useState([]);
   const { addLink, finishedSong } = usePlaylistEmitters();
   const { playlist } = usePlayListState();
 
@@ -15,8 +19,8 @@ const Splash = () => {
 
   const submit = e => {
     e.preventDefault();
-    addLink(link);
     setLink('');
+    videoSearch(`${link} karaoke`);
   };
 
   const opts = {
@@ -28,12 +32,33 @@ const Splash = () => {
     finishedSong();
   };
 
+  const videoSearch = (term) => {
+    YTSearch({ key: superSecretCode, term }, (videos) => {
+      setSearchList(videos.map(i => {
+        const { thumbnails, title } = i.snippet;
+        const { videoId } = i.id;
+        const image = thumbnails.medium ? thumbnails.medium.url : thumbnails.default.url;
+
+        return {
+          image,
+          title,
+          videoId
+        };
+      }));
+    });
+  };
+
+  const addToPlayList = (song) => {
+    addLink(song);
+    setSearchList([]);
+  };
+
   return (
     <section className={styles.Splash}>
-      <h1>Welcome</h1>
+      <h1>KaraoQueue</h1>
       
       {playlist && <YouTube
-        videoId={playlist[0]}
+        videoId={playlist[0] ? playlist[0].videoId : null }
         opts={opts}
         onEnd={onEnd}
       />}
@@ -44,7 +69,16 @@ const Splash = () => {
       </form>
       <ul>
         {playlist && playlist.map(i => {
-          return <li key={i} >{i}</li>;
+          return <li key={i} >{i.title}</li>;
+        })}
+      </ul>
+      <ul>
+        {searchList.map(i => {
+          return (
+            <li key={i.channelId} onClick={() => addToPlayList(i)}>
+              <img src={i.image}/>
+              <h2>{i.title}</h2>
+            </li>);
         })}
       </ul>
     </section>
