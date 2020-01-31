1d98308c88e1c84d66178f00ed08f7ef8aceac08
UncleJerry <jared.myhrberg@gmail.com>
2020-01-31 12:27:36 -0800

    cssing

--- src/components/pages/Concert.js
+++ src/components/pages/Concert.js
@@ -80,11 +80,26 @@ const Concert = ({ match }) => {
 
   const deleteFromPlaylist = data => removeSong(data);
 
-  console.log(playlist);
+  const copyLink = e => {
+    e.preventDefault();
+    var dummy = document.createElement('input'),
+      text = window.location.href;
+
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+  };
+
   return (
     <section className={styles.Concert}>
       <Link to='/'>KaraoQueue</Link>
-      {playlist && <h1>{playlist.roomName}&rsquo;s Concert</h1>}
+      {playlist && <header>
+        <h1>{playlist.roomName}</h1>
+        <h3>Shareable Link :</h3>
+        <h3 id="link" className={styles.Link} onClick={copyLink}>{window.location.href}</h3>
+      </header>}
       {playlist && <YouTube
         videoId={playlist.playlist[0] ? playlist.playlist[0].song.videoId : null }
         onEnd={onEnd}
@@ -93,7 +108,7 @@ const Concert = ({ match }) => {
       <button 
         className={styles['New-Song']}
         onClick={open}
-      >Add Song</button>
+      >Add Song to the Queue</button>
 
       {modal && <section className={styles.Modal}>
         <button className={styles.Exit} onClick={close}>X</button>
